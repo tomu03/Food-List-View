e72fdc34bcfea58ea96dc8f14d0248359265e78a
tanjidul <369tomu@gmail.com>
2024-04-28 10:24:06 +0600

    my food adatter added to main

--- app/src/main/java/com/example/foodlist/MainActivity.kt
+++ app/src/main/java/com/example/foodlist/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.example.foodlist
 
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+
+lateinit var foodView: ListView
+
+val argfoodName = arrayOf(
+    "Pizza", "Burger", "Taco", "Hot Dog", "Milkshake", "Fries", "Sandwich", "Roast chicken", "Steak"
+)
+
+val argfoodDetails = arrayOf(
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+    "Fast food item",
+
+    )
+
+val argfoodImg = arrayOf(
+    R.drawable.pizza,
+    R.drawable.burger,
+    R.drawable.taco,
+    R.drawable.hot_dog,
+    R.drawable.milkshake,
+    R.drawable.fries,
+    R.drawable.sandwich,
+    R.drawable.roast_chicken,
+    R.drawable.steak
+)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        foodView = findViewById(R.id.listView)
+
+        var MyfoodAdapter = myfoodAdapter(this, argfoodName, argfoodDetails, argfoodImg)
+
+        foodView.adapter = MyfoodAdapter
+        foodView.setOnItemClickListener { parent, view, position, id ->
+
+            Toast.makeText(
+                this,
+                "Food Item Clicked${parent.getItemAtPosition(position)}",
+                android.widget.Toast.LENGTH_SHORT
+            )
+
+
         }
+
     }
 }
---
+++ app/src/main/java/com/example/foodlist/myfoodAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.foodlist
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class myfoodAdapter(
+    private val context: Activity,
+    private val title: Array<String>,
+    private val desc: Array<String>,
+    private val imageId: Array<Int>
+) : ArrayAdapter<String>(context,R.layout.listfood, title) {
+    @SuppressLint("MissingInflatedId")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val conInFlater = context.layoutInflater
+        val rowView = conInFlater.inflate(R.layout.listfood, parent, false)
+
+        val titelTxt = rowView.findViewById<TextView>(R.id.foodName)
+        val desctxt = rowView.findViewById<TextView>(R.id.foodDetalis)
+        val image = rowView.findViewById<ImageView>(R.id.foodImg)
+
+        titelTxt.text = title[position]
+        desctxt.text = desc[position]
+        image.setImageResource(imageId[position])
+
+        return rowView
+
+    }
+}
